c5f9441d55303f944bfa423ac320f6ddb51d8f2a
Alexandre Quessy <alexandre@quessy.net>
2010-08-01 01:02:00 -0400

    simplified more the clutter proto

--- src/gtk-clutter-proto.cpp
+++ src/gtk-clutter-proto.cpp
@@ -8,7 +8,6 @@
 #include <glib/gi18n.h>
 #include <clutter-gtk/clutter-gtk.h>
 
-#define TRAILS 0
 #define NHANDS  2
 #define WINWIDTH 400
 #define WINHEIGHT 400
@@ -18,30 +17,17 @@ void proto_setup();
 
 typedef struct SuperOH
 {
-  ClutterActor *hand[NHANDS], *bgtex;
+  ClutterActor *hand[NHANDS];
   ClutterGroup   *group;
   GdkPixbuf      *bgpixb;
-
 } SuperOH; 
 
-static gboolean fade = FALSE;
 static gboolean fullscreen = FALSE;
 
 /* input handler */
-void input_cb(ClutterStage *stage, ClutterEvent *event, gpointer data)
+void on_key_or_button(ClutterStage *stage, ClutterEvent *event, gpointer data)
 {
-    if (event->type == CLUTTER_BUTTON_PRESS)
-    {
-        ClutterActor *a;
-        gfloat x, y;
-
-        clutter_event_get_coords(event, &x, &y);
-
-        a = clutter_stage_get_actor_at_pos(stage, CLUTTER_PICK_ALL, x, y);
-        if (a && (CLUTTER_IS_TEXTURE(a) || CLUTTER_IS_CLONE(a)))
-	        clutter_actor_hide(a);
-    }
-    else if (event->type == CLUTTER_KEY_PRESS)
+    if (event->type == CLUTTER_KEY_PRESS)
     {
         g_print ("*** key press event (key:%c) ***\n", clutter_event_get_key_symbol (event));
         if (clutter_event_get_key_symbol(event) == CLUTTER_q)
@@ -56,13 +42,6 @@ void frame_cb(ClutterTimeline *timeline, gint msecs, gpointer data)
     gint i;
     guint rotation = clutter_timeline_get_progress(timeline) * 360.0f;
   
-#if TRAILS
-    oh->bgpixb = clutter_stage_snapshot(CLUTTER_STAGE(stage), 0, 0, WINWIDTH, WINHEIGHT);
-    clutter_texture_set_pixbuf(CLUTTER_TEXTURE (oh->bgtex), oh->bgpixb);
-    g_object_unref(G_OBJECT (oh->bgpixb));
-    g_object_unref(stage);
-#endif
-
     /* Rotate everything clockwise about stage center*/
     clutter_actor_set_rotation(CLUTTER_ACTOR(oh->group), CLUTTER_Z_AXIS, rotation, WINWIDTH / 2, WINHEIGHT / 2, 0);
   
@@ -76,22 +55,10 @@ void frame_cb(ClutterTimeline *timeline, gint msecs, gpointer data)
             clutter_actor_get_width(oh->hand[i]) / 2,
             clutter_actor_get_height(oh->hand[i]) / 2,
             0);
-        if (fade == TRUE)
-            clutter_actor_set_opacity(oh->hand[i], (255 - (rotation % 255)));
     }
-    /*
-    clutter_actor_rotate_x (CLUTTER_ACTOR(oh->group),
-  			    75.0,
-  			    WINHEIGHT/2, 0);
-    */
 }
 
-static void clickity(GtkButton *button, gpointer ud)
-{
-    fade = !fade;
-}
-
-static void on_fullscreen(GtkButton *button, GtkWindow *window)
+static void on_fullscreen_clicked(GtkButton *button, GtkWindow *window)
 {
     if (!fullscreen)
     {
@@ -101,20 +68,6 @@ static void on_fullscreen(GtkButton *button, GtkWindow *window)
         gtk_window_unfullscreen(window);
         fullscreen = FALSE;
     }
-}
-
-int main(int argc, char *argv[])
-{
-  GError *error;
-
-  error = NULL;
-  gtk_clutter_init_with_args (&argc, &argv, NULL, NULL, NULL, &error);
-  if (error)
-      g_error ("Unable to initialize: %s", error->message);
-
-  proto_setup();
-  gtk_main();
-  return 0;
 }
 
 void proto_setup()
@@ -144,19 +97,15 @@ void proto_setup()
   gtk_container_add(GTK_CONTAINER(vbox), clutter);
 
   stage = gtk_clutter_embed_get_stage(GTK_CLUTTER_EMBED(clutter));
-
+  // label:
   label = gtk_label_new("This is a label");
   gtk_box_pack_start(GTK_BOX(vbox), label, FALSE, FALSE, 0);
-
-  button = gtk_button_new_with_label("This is a button...clicky");
-  g_signal_connect(button, "clicked", G_CALLBACK (clickity), NULL);
-  gtk_box_pack_start(GTK_BOX (vbox), button, FALSE, FALSE, 0);
-
+  // fullscreen button:
   button = gtk_button_new_with_label("Fullscreen");
   gtk_button_set_image(GTK_BUTTON(button), gtk_image_new_from_stock(GTK_STOCK_FULLSCREEN, GTK_ICON_SIZE_BUTTON));
-  g_signal_connect(button, "clicked", G_CALLBACK(on_fullscreen), window);
+  g_signal_connect(button, "clicked", G_CALLBACK(on_fullscreen_clicked), window);
   gtk_box_pack_start(GTK_BOX(vbox), button, FALSE, FALSE, 0);
-
+  // quit button:
   button = gtk_button_new_from_stock(GTK_STOCK_QUIT);
   g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
   gtk_box_pack_end(GTK_BOX (vbox), button, FALSE, FALSE, 0);
@@ -166,13 +115,6 @@ void proto_setup()
   clutter_stage_set_color(CLUTTER_STAGE(stage), &stage_color);
 
   oh = g_new(SuperOH, 1);
-
-#if TRAILS
-  oh->bgtex = clutter_texture_new();
-  clutter_actor_set_size(oh->bgtex, WINWIDTH, WINHEIGHT);
-  clutter_actor_set_opacity(oh->bgtex, 0x99);
-  clutter_group_add(CLUTTER_GROUP (stage), oh->bgtex);
-#endif
 
   /* create a new group to hold multiple actors in a group */
   oh->group = CLUTTER_GROUP(clutter_group_new ());
@@ -208,8 +150,7 @@ void proto_setup()
   /* Add the group to the stage */
   clutter_container_add_actor(CLUTTER_CONTAINER(stage), CLUTTER_ACTOR(oh->group));
 
-  g_signal_connect(stage, "button-press-event", G_CALLBACK(input_cb), oh);
-  g_signal_connect (stage, "key-release-event", G_CALLBACK(input_cb), oh);
+  g_signal_connect (stage, "key-release-event", G_CALLBACK(on_key_or_button), oh);
 
   gtk_widget_show_all(window);
 
@@ -228,5 +169,19 @@ void proto_setup()
 
   /* and start it */
   clutter_timeline_start(timeline);
+}
+
+int main(int argc, char *argv[])
+{
+  GError *error;
+
+  error = NULL;
+  gtk_clutter_init_with_args (&argc, &argv, NULL, NULL, NULL, &error);
+  if (error)
+      g_error ("Unable to initialize: %s", error->message);
+
+  proto_setup();
+  gtk_main();
+  return 0;
 }
 
