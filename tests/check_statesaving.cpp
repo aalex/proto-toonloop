e241cba5e6b06b68cf4ef4487e752f463aeaede2
Alexandre Quessy <alexandre@quessy.net>
2010-09-04 21:54:50 -0400

    adding a prototype using tinyxml

--- tests/check_statesaving.cpp
+++ tests/check_statesaving.cpp
@@ -1,5 +1,21 @@
+#include <iostream>
+#include "tinyxml.h"
+void build_simple_doc( )
+{
+	// Make xml: <?xml ..><Hello>World</Hello>
+	TiXmlDocument doc;
+	TiXmlDeclaration * decl = new TiXmlDeclaration( "1.0", "", "" );
+	TiXmlElement * element = new TiXmlElement( "Hello" );
+	TiXmlText * text = new TiXmlText( "World" );
+	element->LinkEndChild( text );
+	doc.LinkEndChild( decl );
+	doc.LinkEndChild( element );
+	doc.SaveFile( "madeByHand.xml" );
+}
+
 int main()
 {
+    build_simple_doc();
     return 0;
 }
 
