b36a5a1cb8320b547fa081560243ffd8cbfa9acb
Alexandre Quessy <alexandre@quessy.net>
2010-10-30 13:01:51 -0400

    It compiles, but the shader doesn't work

--- main.c
+++ main.c
@@ -3,18 +3,61 @@
 #define PKGDATADIR "./data/"
 #define IMGFILE "example.jpg" // works with svg too
 
+static gchar *frag_source = "uniform float radius ;"
+        "uniform sampler2DRect rectTexture;"
+        ""
+        "void main()"
+        "{"
+        "    vec4 color = texture2DRect(rectTexture, gl_TexCoord[0].st);"
+        "    float u;"
+        "    float v;"
+        "    int count = 1;"
+        "    for (u=-radius;u<radius;u++)"
+        "      for (v=-radius;v<radius;v++)"
+        "        {"
+        "          color += texture2DRect(rectTexture, vec2(gl_TexCoord[0].s + u * 2, gl_TexCoord[0].t +v * 2));"
+        "          count ++;"
+        "        }"
+        ""
+        "    gl_FragColor = color / count;"
+        "}" ;
+
 static ClutterActor *load_image(int width, int height)
 {
     ClutterActor *actor = NULL;
     actor = clutter_texture_new ();
     gboolean success;
     GError *error = NULL;
-    success = clutter_texture_set_from_file(CLUTTER_TEXTURE(actor), PKGDATADIR IMGFILE, &error);
+    gchar *file_name = PKGDATADIR IMGFILE;
+    success = clutter_texture_set_from_file(CLUTTER_TEXTURE(actor), file_name, &error);
     if (! success)
-        g_print("Could not load image.\n");
+        g_print("Could not load image %s.\n", file_name);
     if (error)
-        g_error("Error loading image: %s\n", error->message);
+        g_error("Error loading image %s: %s\n", file_name, error->message);
     return actor;
+}
+
+static void setup_shader(ClutterActor *actor)
+{
+    ClutterShader *shader = NULL;
+    shader = clutter_shader_new();
+    clutter_shader_set_fragment_source(shader, frag_source, -1);
+    GError *error = NULL;
+    clutter_shader_compile(shader, &error);
+    if (error)
+    {
+        g_warning("Unable to init shader: %s", error->message);
+        g_error_free(error);
+    }
+    else
+    {
+        clutter_actor_set_shader(actor, shader);
+        GValue value = { 0, };
+        g_value_init(&value, G_TYPE_FLOAT);
+        g_value_set_float(&value, 5.0);
+        clutter_actor_set_shader_param(actor, "radius", &value);
+        g_value_unset(&value);
+    }
 }
 
 int main(int argc, char *argv[])
@@ -28,6 +71,7 @@ int main(int argc, char *argv[])
     
     ClutterActor *image = load_image(width, height);
     clutter_container_add_actor(CLUTTER_CONTAINER(stage), image);
+    setup_shader(image);
     clutter_actor_show_all(stage);
     clutter_main();
     return 0;
